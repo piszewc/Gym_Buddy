fca3a93deaf38ee77bf3ef3f899885e67ea6c4c8
Piotr Szewc <39332276+piszewc@users.noreply.github.com>
2018-11-29 21:47:15 +0100

    base buddy done

--- weight_buddy/views.py
+++ weight_buddy/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Training
 
 # Create your views here.
 def weight_list(request):
-    return render(request, 'weight_buddy/weight_list.html', {})
+    trainings = Training.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'weight_buddy/weight_list.html', {'trainings': trainings})
