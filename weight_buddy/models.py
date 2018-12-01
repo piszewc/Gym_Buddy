bf9b38cf7f194f7af9579764928f563c7a58fb06
Piotr Szewc <39332276+piszewc@users.noreply.github.com>
2018-12-01 22:08:16 +0100

    gym update

--- weight_buddy/models.py
+++ weight_buddy/models.py
@@ -29,7 +29,7 @@ class Exercises(models.Model):
 	def __str__(self):
 		return "%s %s" % (self.excercise_name, self.set_repetitions_number )	
 
-DEFAULT_EXERCISE_ID = 1
+
 
 class Training(models.Model):
 
@@ -43,25 +43,46 @@ class Training(models.Model):
         choices=workout_type_choices,
         default=PUSH,
     )
-	
+	DEFAULT_EXERCISE_ID_ONE = 1
+	DEFAULT_EXERCISE_ID_TWO = 2
+	DEFAULT_EXERCISE_ID_TREE = 3
+	DEFAULT_EXERCISE_ID_FOUR = 4
+	DEFAULT_EXERCISE_ID_FIVE = 5
+	DEFAULT_EXERCISE_ID_SIX = 6
+
+	if workout_type==PUSH:
+		DEFAULT_EXERCISE_ID_ONE = 1
+		DEFAULT_EXERCISE_ID_TWO = 2
+		DEFAULT_EXERCISE_ID_TREE = 3
+		DEFAULT_EXERCISE_ID_FOUR = 4
+		DEFAULT_EXERCISE_ID_FIVE = 5
+		DEFAULT_EXERCISE_ID_SIX = 6
+	elif workout_type==PULL:
+		DEFAULT_EXERCISE_ID_ONE = 1+6
+		DEFAULT_EXERCISE_ID_TWO = 2+6
+		DEFAULT_EXERCISE_ID_TREE = 3+6
+		DEFAULT_EXERCISE_ID_FOUR = 4+6
+		DEFAULT_EXERCISE_ID_FIVE = 5+6
+		DEFAULT_EXERCISE_ID_SIX = 6+6
+
 	workout_name =models.CharField(max_length=200)
 
-	exercise = models.ForeignKey(Exercises, on_delete=models.SET_DEFAULT, default=DEFAULT_EXERCISE_ID, related_name='exercise')
+	exercise = models.ForeignKey(Exercises, on_delete=models.SET_DEFAULT, default=DEFAULT_EXERCISE_ID_ONE, related_name='exercise')
 	weight = models.SmallIntegerField(blank=True, null=True)
 
-	exercise_one = models.ForeignKey(Exercises, on_delete=models.SET_DEFAULT, default=DEFAULT_EXERCISE_ID, related_name='exercise_one')
+	exercise_one = models.ForeignKey(Exercises, on_delete=models.SET_DEFAULT, default=DEFAULT_EXERCISE_ID_TWO, related_name='exercise_one')
 	weight_one = models.SmallIntegerField(blank=True, null=True)
 
-	exercise_two = models.ForeignKey(Exercises, on_delete=models.SET_DEFAULT, default=DEFAULT_EXERCISE_ID, related_name='exercise_two')
+	exercise_two = models.ForeignKey(Exercises, on_delete=models.SET_DEFAULT, default=DEFAULT_EXERCISE_ID_TREE, related_name='exercise_two')
 	weight_two = models.SmallIntegerField(blank=True, null=True)
 
-	exercise_tree = models.ForeignKey(Exercises, on_delete=models.SET_DEFAULT, default=DEFAULT_EXERCISE_ID, related_name='exercise_tree')
+	exercise_tree = models.ForeignKey(Exercises, on_delete=models.SET_DEFAULT, default=DEFAULT_EXERCISE_ID_FOUR, related_name='exercise_tree')
 	weight_tree = models.SmallIntegerField(blank=True, null=True)
 
-	exercise_four = models.ForeignKey(Exercises, on_delete=models.SET_DEFAULT, default=DEFAULT_EXERCISE_ID, related_name='exercise_four')
+	exercise_four = models.ForeignKey(Exercises, on_delete=models.SET_DEFAULT, default=DEFAULT_EXERCISE_ID_FIVE, related_name='exercise_four')
 	weight_four = models.SmallIntegerField(blank=True, null=True)
 
-	exercise_five = models.ForeignKey(Exercises, on_delete=models.SET_DEFAULT, default=DEFAULT_EXERCISE_ID, related_name='exercise_five')
+	exercise_five = models.ForeignKey(Exercises, on_delete=models.SET_DEFAULT, default=DEFAULT_EXERCISE_ID_SIX, related_name='exercise_five')
 	weight_five = models.SmallIntegerField(blank=True, null=True)
 
 	created_date = models.DateTimeField(
