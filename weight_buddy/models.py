29043d4e046c0dd6535792373123f56f98183014
Piotr Szewc <39332276+piszewc@users.noreply.github.com>
2018-12-04 22:11:08 +0100

    Updated Buddy!

--- weight_buddy/models.py
+++ weight_buddy/models.py
@@ -29,7 +29,8 @@ class Exercises(models.Model):
 	def __str__(self):
 		return "%s %s" % (self.excercise_name, self.set_repetitions_number )	
 
-
+class ExercisesSet(models.Model):
+	
 
 class Training(models.Model):
 
@@ -50,21 +51,6 @@ class Training(models.Model):
 	DEFAULT_EXERCISE_ID_FOUR = 4
 	DEFAULT_EXERCISE_ID_FIVE = 5
 	DEFAULT_EXERCISE_ID_SIX = 6
-
-	if workout_type==PUSH:
-		DEFAULT_EXERCISE_ID_ONE = 1
-		DEFAULT_EXERCISE_ID_TWO = 2
-		DEFAULT_EXERCISE_ID_TREE = 3
-		DEFAULT_EXERCISE_ID_FOUR = 4
-		DEFAULT_EXERCISE_ID_FIVE = 5
-		DEFAULT_EXERCISE_ID_SIX = 6
-	elif workout_type==PULL:
-		DEFAULT_EXERCISE_ID_ONE = 1+6
-		DEFAULT_EXERCISE_ID_TWO = 2+6
-		DEFAULT_EXERCISE_ID_TREE = 3+6
-		DEFAULT_EXERCISE_ID_FOUR = 4+6
-		DEFAULT_EXERCISE_ID_FIVE = 5+6
-		DEFAULT_EXERCISE_ID_SIX = 6+6
 
 	workout_name =models.CharField(max_length=200)
 
