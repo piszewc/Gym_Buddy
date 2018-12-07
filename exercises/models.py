61e14aad923cc7c4d2dcdfd19a8f22ccec57f267
Piotr Szewc <39332276+piszewc@users.noreply.github.com>
2018-12-07 23:59:38 +0100

    models updated

--- exercises/models.py
+++ exercises/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class ExercisesList(models.Model):
+    exercise_name
+    equipment
+    exercise_type
+    major_muscle
+    minor_muscle
+    description
+    example
+    notes
+    modifications
