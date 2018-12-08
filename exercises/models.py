e495205a8500334b309e1aa2ca3e7747a79a09a2
Piotr Szewc <39332276+piszewc@users.noreply.github.com>
2018-12-08 17:38:18 +0100

    updated models

--- exercises/models.py
+++ exercises/models.py
@@ -2,12 +2,104 @@ from django.db import models
 
 # Create your models here.
 class ExercisesList(models.Model):
-    exercise_name
-    equipment
-    exercise_type
-    major_muscle
-    minor_muscle
-    description
-    example
-    notes
-    modifications
+
+
+class Equipment(models.Model):
+     '''
+    Equipment used or needed by an exercise
+    '''
+
+    name = models.CharField(max_length=50,
+                            verbose_name=_('Name'))
+
+    class Meta:
+        '''
+        Set default ordering
+        '''
+        ordering = ["name", ]
+
+    def __str__(self):
+        '''
+        Return a more human-readable representation
+        '''
+        return self.name
+
+    def get_owner_object(self):
+        '''
+        Equipment has no owner information
+        '''
+        return False
+
+class MajorMuscle(models.Model):
+
+   name = models.CharField(max_length=50,
+                            verbose_name=_('Name'),
+                            help_text=_('Major Muscle'))
+
+    # Whether to use the front or the back image for background
+    is_front = models.BooleanField(default=1)
+
+    # Metaclass to set some other properties
+    class Meta:
+        ordering = ["name", ]
+
+    def __str__(self):
+        '''
+        Return a more human-readable representation
+        '''
+        return self.name
+
+    def get_owner_object(self):
+        '''
+        Muscle has no owner information
+        '''
+        return False
+
+class MinorMuscle(models.Model):
+    name = models.CharField(max_length=50,
+                            verbose_name=_('Name'),
+                            help_text=_('Minor Muscle'))
+
+    # Whether to use the front or the back image for background
+    is_front = models.BooleanField(default=1)
+
+    # Metaclass to set some other properties
+    class Meta:
+        ordering = ["name", ]
+
+    def __str__(self):
+        '''
+        Return a more human-readable representation
+        '''
+        return self.name
+
+    def get_owner_object(self):
+        '''
+        Muscle has no owner information
+        '''
+        return False
+
+class ExerciseType(models.Model):
+    name = models.CharField(max_length=50,
+                        verbose_name=_('Name'),
+                        help_text=_('Exercise Type'))
+
+    # Whether to use the front or the back image for background
+    is_front = models.BooleanField(default=1)
+
+    # Metaclass to set some other properties
+    class Meta:
+        ordering = ["name", ]
+
+    def __str__(self):
+        '''
+        Return a more human-readable representation
+        '''
+        return self.name
+
+    def get_owner_object(self):
+        '''
+        Muscle has no owner information
+        '''
+        return False
+
