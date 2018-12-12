db9e8a2ab4f4a094cf101f27ebbb7746427df2f8
Piotr Szewc <39332276+piszewc@users.noreply.github.com>
2018-12-12 23:22:12 +0100

    Updated Ex Type

--- exercises/views/exercise_type.py
+++ exercises/views/exercise_type.py
@@ -1,23 +1,18 @@
-class ExerciseTypeListView(ListView):
+class ExerciseTypeListView(ListView, models.Model):
 
-    CHEST = 'CHEST'
-    DELTS = 'DELTS'
-    TRICEPS = 'TRICEPS'
-    LEGS = 'LEGS'
-    ABS = 'ABS'
-    BACK = 'BACK'
-    BICEPS = 'BICEPS'
-    FOREARMS = 'FOREARMS'
+    CARDIO = 'CR'
+    PLYO = 'PL'
+    WEIGHT = 'WG'
+    LAPS = 'LP'
 
-    excercise_type_choices = ((CHEST, 'CHEST'),(DELTS, 'DELTS'),(TRICEPS, 'TRICEPS')
-    ,(BACK, 'BACK'),(BICEPS, 'BICEPS'),(FOREARMS, 'FOREARMS')
-    ,(LEGS, 'LEGS'),(ABS, 'ABS'),)
+    excercise_type_choices = ((CARDIO, 'CARDIO'),(PLYO, 'PLYO'),(WEIGHT, 'WEIGHT')
+    ,(LAPS, 'LAPS'),)
 
     workout_type = models.CharField(
-        max_length=10,
+        max_length=2,
         choices=excercise_type_choices,
-        default=CHEST,
+        default=CARDIO,
     )
 
-	def __str__(self):
-		return "%s %s" % (self.workout_type)	
+    def __str__(self):
+        return "%s" % (self.workout_type)	
